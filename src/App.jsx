6cb3250b93ec1297defb78498d3af6aa53e77e86
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-25 11:40:48 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Application for task management</h1>
+        </header>
+        <div className="App-body">
+            This is body
+        </div>
+          <footer className="App-footer">
+              <h1 className="App-title">Footer for app</h1>
+          </footer>
+      </div>
+
+    );
+  }
+}
+
+export default App;
